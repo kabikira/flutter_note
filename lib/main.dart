695e2bd1c310a9164fbb9b46bd0fbe035eeeffda
kabikira <kabikirar2@protonmail.com>
2025-01-27 13:43:31 +0900

    #2a

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  const b = 'バナナ';
-  const t = Text(b);
-  const c = Center(child: t);
-  const s = Scaffold(body: c);
-  const a = MaterialApp(home: s);
+  const a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text('バナナ'),
+      ),
+    ),
+  );
   runApp(a);
 }
