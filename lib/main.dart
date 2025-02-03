9e7a4a18b1d5b13bf1c7c1e7025c91b44f3e2ce0
kabikira <kabikirar2@protonmail.com>
2025-02-03 13:25:06 +0900

    #7Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_note/banana_counter.dart';
+
+/*
+  最新のFlutterに対応するため、動画と少しコードが変わりました
+*/
 
 void main() {
-  final b = BananaCounter(
-    number: 888,
+  xxxx() {
+    debugPrint('これから通信を始めます');
+    debugPrint('通信中です');
+    debugPrint('通信が終わりました');
+  }
+
+  final button = ElevatedButton(
+    onPressed: xxxx,
+    style: ElevatedButton.styleFrom(
+      // MEMO: primary は古くなったので backgroundColor へ変更しました
+      backgroundColor: Colors.green,
+    ),
+    child: const Text('押してみて'),
   );
+
   final a = MaterialApp(
     home: Scaffold(
       body: Center(
-        child: b,
+        child: button,
       ),
     ),
   );
+
   runApp(a);
 }
