a5908678c6eb971c61fadb56986d695a8ef72439
kabikira <kabikirar2@protonmail.com>
2025-02-05 13:10:52 +0900

    #12Drawer編

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_note/page_a.dart';
-import 'package:flutter_note/page_b.dart';
-import 'package:flutter_note/page_c.dart';
-import 'package:go_router/go_router.dart';
-
-/*
-  最新のFlutterに対応するため、動画と少しコードが変わりました
-*/
+import 'package:flutter_note/side_menu.dart';
 
 main() {
-  final app = App();
-  runApp(app);
-}
-
-// アプリ全体
-class App extends StatelessWidget {
-  App({super.key});
-
-  final router = GoRouter(
-    // パス (アプリが起動したとき)
-    initialLocation: '/a',
-    // パスと画面の組み合わせ
-    routes: [
-      GoRoute(
-        path: '/a',
-        builder: (context, state) => const PageA(),
-      ),
-      GoRoute(
-        path: '/b',
-        builder: (context, state) => const PageB(),
-      ),
-      GoRoute(
-        path: '/c',
-        builder: (context, state) => const PageC(),
-      ),
-    ],
-    redirect: (context, state) {
-      print("現在のパス: ${state.fullPath}");
-      return null;
-    },
+  // アップバー
+  final appBar = AppBar(
+    title: const Text('appBar'),
   );
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationProvider: router.routeInformationProvider,
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
-    );
-  }
+  // ドロワー
+  const drawer = Drawer(
+    child: SideMenu(),
+  );
+
+  // エンドドロワー
+  const endDrawer = Drawer(
+    child: SideMenu(),
+  );
+
+  // フローティングアクションボタン (FAB)
+  final fab = FloatingActionButton(
+    onPressed: () {
+      debugPrint('FAB が押されました');
+    },
+    child: const Text('FAB'),
+  );
+
+  // ボディ
+  const body = Center(
+    child: Text('body'),
+  );
+
+  // 画面
+  final scaffold = Scaffold(
+    appBar: appBar, // アップバー
+    drawer: drawer, // ドロワー
+    endDrawer: endDrawer, // エンドドロワー
+    floatingActionButton: fab, // フローティングアクションボタン (FAB)
+    body: body, // ボディ
+  );
+
+  // アプリ
+  final app = MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: scaffold,
+  );
+
+  runApp(app);
 }
